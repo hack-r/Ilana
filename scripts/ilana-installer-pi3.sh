4058c13196f120fcaf313158981ef3704b2abece
Jason D. Miller <millerintllc@gmail.com>
2018-02-20 02:40:27 +0000

    misc renames from upstream audio fork

--- scripts/ilana-installer-pi3.sh
+++ scripts/ilana-installer-pi3.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-# Copyright 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -o errexit
 
 scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
@@ -38,15 +24,15 @@ echo "Your Model-Id used for the project is: $modelid" >> /home/pi/modelid.txt
 cd /home/pi/
 sudo apt-get update -y
 
-sed 's/#.*//' /home/pi/GassistPi/Requirements/GassistPi-system-requirements.txt | xargs sudo apt-get install -y
+sed 's/#.*//' /home/pi/Desktop/Ilana/Requirements/Ilana-system-requirements.txt | xargs sudo apt-get install -y
 if [ ! -d /home/pi/.config/mpv/scripts/ ]; then
   mkdir -p /home/pi/.config/mpv/scripts/
 fi
-if [ -f /home/pi/GassistPi/src/end.lua ]; then
-  mv /home/pi/GassistPi/src/end.lua /home/pi/.config/mpv/scripts/end.lua
+if [ -f /home/pi/Desktop/Ilana/src/end.lua ]; then
+  mv /home/pi/Desktop/Ilana/src/end.lua /home/pi/.config/mpv/scripts/end.lua
 fi
-if [ -f /home/pi/GassistPi/src/mpv.conf ]; then
-  mv /home/pi/GassistPi/src/mpv.conf /home/pi/.config/mpv/mpv.conf
+if [ -f /home/pi/Desktop/Ilana/src/mpv.conf ]; then
+  mv /home/pi/Desktop/Ilana/src/mpv.conf /home/pi/.config/mpv/mpv.conf
 fi
 
 
@@ -56,6 +42,7 @@ source env/bin/activate
 
 pip install -r /home/pi/GassistPi/Requirements/GassistPi-pip-requirements.txt
 
+pip install google-assistant-library==0.1.0
 pip install google-assistant-grpc==0.1.0
 pip install google-assistant-sdk==0.4.2
 pip install google-assistant-sdk[samples]==0.4.2
@@ -63,5 +50,5 @@ pip install google-auth==1.3.0	google-auth-httplib2==0.0.3 google-auth-oauthlib=
 google-oauthlib-tool --client-secrets /home/pi/$credname --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
 googlesamples-assistant-devicetool register-model --manufacturer "Pi Foundation" \
           --product-name $prodname --type LIGHT --model $modelid
-echo "Testing the installed google assistant. Make a note of the generated Device-Id"
-googlesamples-assistant-pushtotalk --project-id $projid --device-model-id $modelid
+echo "While I'm testing the installed google ass., you ake a note of the generated Device-Id"
+googlesamples-assistant-hotword --project_id $projid --device_model_id $modelid
