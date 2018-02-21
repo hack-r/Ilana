4629fc6fbb258d2aed6a40565b60d8ac9c6a47d5
Jason D. Miller <millerintllc@gmail.com>
2018-02-21 18:41:29 +0000

    misc improvements to installer

--- scripts/ilana-installer-pi3.sh
+++ scripts/ilana-installer-pi3.sh
@@ -22,8 +22,10 @@ echo ""
 modelid=$projid-$(date +%Y%m%d%H%M%S )
 echo "Your Model-Id used for the project is: $modelid" >> /home/pi/modelid.txt
 cd /home/pi/
+echo "Ilana is updating apt..."
 sudo apt-get update -y
 
+echo "Ilana is compiling and installing dependencies..."
 sed 's/#.*//' /home/pi/Desktop/Ilana/Requirements/Ilana-system-requirements.txt | xargs sudo apt-get install -y
 if [ ! -d /home/pi/.config/mpv/scripts/ ]; then
   mkdir -p /home/pi/.config/mpv/scripts/
@@ -35,20 +37,24 @@ if [ -f /home/pi/Desktop/Ilana/src/mpv.conf ]; then
   mv /home/pi/Desktop/Ilana/src/mpv.conf /home/pi/.config/mpv/mpv.conf
 fi
 
-
+echo "Ilana is setting up a Python 3.x virtual environment..."
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
 source env/bin/activate
 
+echo "Ilana is installing requirements.txt into the virtual env..."
 pip install -r /home/pi/Desktop/Ilana/Requirements/Ilana-pip-requirements.txt
 
-pip install google-assistant-library==0.1.0
-pip install google-assistant-grpc==0.1.0
-pip install google-assistant-sdk==0.4.2
-pip install google-assistant-sdk[samples]==0.4.2
-pip install google-auth==1.3.0	google-auth-httplib2==0.0.3 google-auth-oauthlib==0.2.0
+echo "Ilana is installing Google libraries for Python..."
+pip install google-assistant-library
+pip install google-assistant-grpc
+pip install google-assistant-sdk
+pip install google-assistant-sdk[samples]
+
+echo "Ilana is installing and implementing authentication..."
+pip install google-auth	google-auth-httplib2 google-auth-oauthlib
 google-oauthlib-tool --client-secrets /home/pi/$credname --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
 googlesamples-assistant-devicetool register-model --manufacturer "Pi Foundation" \
-          --product-name $prodname --type LIGHT --model $modelid
-echo "While I'm testing the installed google ass., you ake a note of the generated Device-Id"
-googlesamples-assistant-hotword --project_id $projid --device_model_id $modelid
+          --product-name $prodname --type LIGHT --model $modelid googlesamples-assistant-hotword --project_id $projid --device_model_id $modelid
+
+echo "Ilana setup complete!"
